f136b62a4b2a4614185054db5e15a1ddc28fba1f
Kacper-Mila <kacper.mila@gmail.com>
2025-01-11 19:45:18 +0100

    initial commit

---
+++ Wordle/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace Wordle;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnDecrementClicked(object sender, EventArgs e)
+    {
+        if (int.TryParse(WordLengthEntry.Text, out int wordLength) && wordLength > 4)
+        {
+            WordLengthEntry.Text = (wordLength - 1).ToString();
+        }
+    }
+
+    private void OnIncrementClicked(object sender, EventArgs e)
+    {
+        if (int.TryParse(WordLengthEntry.Text, out int wordLength) && wordLength < 7)
+        {
+            WordLengthEntry.Text = (wordLength + 1).ToString();
+        }
+    }
+}
